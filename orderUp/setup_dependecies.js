4cc36be32e38a947cf6aefe50224b9f59ff2c2fd
Adrian Roger <adrianroge@gmail.com>
2024-04-14 17:42:55 -0300

    [feat]: create project structure and setup_dependencies.js

---
+++ orderUp/setup_dependecies.js
@@ -0,0 +1,27 @@
+const { execSync } = require('child_process');
+
+// List of project dependencies
+const dependencies = [
+  'nodemon',
+  'express',
+  'dotenv',
+  'cookie-parser',
+  'cors'
+];
+
+// Function to install all dependencies
+function installDependencies() {
+  console.log('Installing dependencies...');
+  dependencies.forEach(dependency => {
+    try {
+      console.log(`Installing ${dependency}`);
+      execSync(`npm install ${dependency}`, { stdio: 'inherit' });
+      console.log(`${dependency} installed successfully.`);
+    } catch (error) {
+      console.error(`Error installing ${dependency}: ${error}`);
+    }
+  });
+}
+
+// Call de function to install dependencies
+installDependencies();
