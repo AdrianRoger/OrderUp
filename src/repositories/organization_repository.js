90899d742de47e6d2a0a31f6c8079b9aaa45aa69
csarfau <csarfau@gmail.com>
2024-04-18 00:18:22 -0300

    [feat]: Add new methods in organization_repository.js

--- src/repositories/organization_repository.js
+++ src/repositories/organization_repository.js
@@ -1,10 +1,10 @@
 const database = require('../database/database.js');
-const Organization = require('../model/organization_model');
+const Organization = require('../model/Organization_model.js');
 
 class OrganizationRepository {
   async getOrganizations() {
     try {
-      const results = await database.excuteQuery({
+      const results = await database.executeQuery({
         query: `SELECT * FROM organization`
       });
 
@@ -30,6 +30,64 @@ class OrganizationRepository {
       console.log("erro aqui");
     }
   }
+
+  async getOrganizationsById(id) {
+    try {
+      const results = await database.executeQuery({
+        query: `SELECT * FROM organization WHERE id = $1`,
+        args: [id]
+      });
+
+      if (results <= 0) {
+        throw new exception("")
+      }
+      const organizations = results.map((result) => {
+        return new Organization({
+          id: result.id,
+          name: result.name,
+          cnpj: result.cnpj,
+          email: result.email,
+          telephone: result.telephone,
+          zipcode: result.zipcode,
+          street: result.street,
+          number: result.number,
+          city: result.city,
+          state: result,state,
+          expireDate: result.expireDate
+        });
+      });
+
+      return organizations ?? [];
+
+    } catch (exception) {
+      console.log("erro aqui");
+    }
+  }
+
+  async createOrganization() {
+    try {
+
+    } catch (exception) {
+
+    }
+  }
+
+  async updateOrganization() {
+    try {
+
+    } catch (exception) {
+
+    }
+  }
+
+  async deleteOrganization() {
+    try {
+
+    } catch (exception) {
+
+    }
+  }
+
 }
 
 module.exports = OrganizationRepository;
