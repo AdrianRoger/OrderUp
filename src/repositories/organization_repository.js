09d6a70a51a7e9f92ae7247eb8cf5a4f1746e4fb
Cesar Faustino <cesar.faustino@pasi.com.br>
2024-04-18 17:25:37 -0300

    [feat]: Create all methods of organization_repository.js

--- src/repositories/organization_repository.js
+++ src/repositories/organization_repository.js
@@ -31,63 +31,124 @@ class OrganizationRepository {
     }
   }
 
-  async getOrganizationsById(id) {
+  async getOrganizationsById(organizationId) {
     try {
-      const results = await database.executeQuery({
+      const result = await database.executeQuery({
         query: `SELECT * FROM organization WHERE id = $1`,
-        args: [id]
+        args: [organizationId]
       });
 
-      if (results <= 0) {
-        throw new exception("")
+      if (result.lenght === 0) {
+        return null;
       }
-      const organizations = results.map((result) => {
-        return new Organization({
-          id: result.id,
-          name: result.name,
-          cnpj: result.cnpj,
-          email: result.email,
-          telephone: result.telephone,
-          zipcode: result.zipcode,
-          street: result.street,
-          number: result.number,
-          city: result.city,
-          state: result,state,
-          expireDate: result.expireDate
-        });
-      });
 
-      return organizations ?? [];
+      const organization = new Organization({
+          id: result[0].id,
+          name: result[0].name,
+          cnpj: result[0].cnpj,
+          email: result[0].email,
+          telephone: result[0].telephone,
+          zipcode: result[0].zipcode,
+          street: result[0].street,
+          number: result[0].number,
+          city: result[0].city,
+          state: result[0].state,
+          expireDate: result[0].expireDate
+        });
+
+      return organization;
 
     } catch (exception) {
       console.log("erro aqui");
     }
   }
 
-  async createOrganization() {
+  async createOrganization(name, cnpj, email, telephone, zipcode, street, number, city, state, expireDate) {
     try {
+      const result = await database.executeQuery({
+        query: 
+          `INSERT INTO organization (id, name, cnpj, email, telephone, zipcode, street, number, city, state, expire_date) 
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *`,
+        args: [name, cnpj, email, telephone, zipcode, street, number, city, state, expireDate]
+      });
 
+      const createdOrganization = new Organization({
+        id: result[0].id,
+        name: result[0].name,
+        cnpj: result[0].cnpj,
+        email: result[0].email,
+        telephone: result[0].telephone,
+        zipcode: result[0].zipcode,
+        street: result[0].street,
+        number: result[0].number,
+        city: result[0].city,
+        state: result[0].state,
+        expireDate: result[0].expireDate
+      });
+
+      return createdOrganization;
     } catch (exception) {
-
+      console.log("erro aqui");
     }
   }
 
-  async updateOrganization() {
+  async updateOrganization(organizationId, name, cnpj, email, telephone, zipcode, street, number, city, state, expireDate) {
     try {
+      const result = await database.executeQuery({
+        query:
+        `UPDATE organization SET name = $2, cnpj = $3, email = $4, telephone = $5, zipcode = $6, street = $7, 
+          number = $8, city = $9, state = $10, expire_date = $11) WHERE id = $1 RETURNING *`,
+        args: [organizationId, name, cnpj, email, telephone, zipcode, street, number, city, state, expireDate]
+      });
 
+      const updatedOrganization = new Organization({
+        id: result[0].id,
+        name: result[0].name,
+        cnpj: result[0].cnpj,
+        email: result[0].email,
+        telephone: result[0].telephone,
+        zipcode: result[0].zipcode,
+        street: result[0].street,
+        number: result[0].number,
+        city: result[0].city,
+        state: result[0].state,
+        expireDate: result[0].expireDate
+      });
+
+      return updatedOrganization;
     } catch (exception) {
-
+      console.log("erro aqui");
     }
   }
 
-  async deleteOrganization() {
+  async deleteOrganization(organizationId) {
     try {
+      const result = await database.executeQuery({
+        query: `DELETE FROM organization WHERE id = $1 RETURNING *`,
+        args: [organizationId]
+      });
 
+      const deletedOrganization = new Organization({
+        id: result[0].id,
+        name: result[0].name,
+        cnpj: result[0].cnpj,
+        email: result[0].email,
+        telephone: result[0].telephone,
+        zipcode: result[0].zipcode,
+        street: result[0].street,
+        number: result[0].number,
+        city: result[0].city,
+        state: result[0].state,
+        expireDate: result[0].expireDate
+      });
+
+      return deletedOrganization;
     } catch (exception) {
-
+      console.log("erro aqui");
     }
   }
-
 }
 
-module.exports = OrganizationRepository;
+const organizationRepository = new OrganizationRepository();
+
+module.exports = organizationRepository;
