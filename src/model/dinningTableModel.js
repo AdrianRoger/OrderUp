1fd7f2c92ad32e3e94511c6cc14e0c826f885b63
Adrian Roger <adrianroge@gmail.com>
2024-04-27 18:01:50 -0300

    [fix]: delete unnecessary files dinningTable(s)

--- src/model/dinningTableModel.js
+++
@@ -1,9 +0,0 @@
-class Dinning{
-    constructor({ id, closed, deviceId }){
-        this.id = id;
-        this.closed = closed;
-        this.deviceId = deviceId;
-    }
-}
-
-module.exports = Dinning;
--- src/repositories/admin_repository.js
+++ src/repositories/admin_repository.js
@@ -1,5 +1,5 @@
 const database = require('../database/database.js');
-const Admin = require('../model/Admin_model.js');
+const Admin = require('../model/AdminModel.js');
 
 // class AdminRepository{
 //   async login({ name, password }) { //trocar name para email
