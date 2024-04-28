f4bede6618a77280967b6273df024b16845f4ce1
csarfau <csarfau@gmail.com>
2024-04-28 20:56:07 -0300

    [feat]: Create method getOrganizationByLoginName in all Organization routes

--- src/routes/organizationRoutes.js
+++ src/routes/organizationRoutes.js
@@ -4,6 +4,7 @@ const organizationController = require('../controller/OrganizationController');
 const organizationRouter = express.Router();
 
 organizationRouter.get('/', organizationController.getOrganizations);
+organizationRouter.get('/loginName/:loginName', organizationController.getOrganizationByLoginName);
 organizationRouter.get('/:id', organizationController.getOrganizationById);
 organizationRouter.post('/', organizationController.createOrganization);
 organizationRouter.put('/:id', organizationController.updateOrganization);
