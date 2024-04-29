9a154548c1a3cfb8663da1095eb35a1cb2e7602b
Adrian Roger <adrianroge@gmail.com>
2024-04-29 09:55:44 -0300

    [feat]: create index.js for repositories folder

---
+++ src/repositories/index.js
@@ -0,0 +1,17 @@
+const adminRepository = require('./ProductRepository');
+const categoryRepository = require('./CategoryRepository');
+const deviceRepository = require('./DeviceRepository');
+const orderingProductRepository = require('./OrderingProductRepository');
+const orderingRepository = require('./OrderingRepository');
+const organizationRepository = require('./OrganizationRepository');
+const productRepository = require('./ProductRepository');
+
+module.exports = {
+  adminRepository,
+  categoryRepository,
+  deviceRepository,
+  orderingProductRepository,
+  orderingRepository,
+  organizationRepository,
+  productRepository,
+}
