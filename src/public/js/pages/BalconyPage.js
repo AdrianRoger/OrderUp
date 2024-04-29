976efc16cffbeff0e004315e573bb7b1f36c8816
Murilo Russo Netto <murilornetto@gmail.com>
2024-04-28 23:31:38 -0300

    [fix] fix commit

---
+++ src/public/js/pages/BalconyPage.js
@@ -0,0 +1,64 @@
+import AbastractPage from "./AbastractPage.js";
+
+
+export default class extends AbastractPage {
+  constructor() {
+    super();
+    this.setTitle("Categories");
+  }
+
+  async getHtml() {
+    const categories = document.createElement("body");
+    categories.id = "categories-list";
+    try {
+     
+
+      const response = await fetch(
+        "/api/categories/a7930dec-36ea-4a8d-998e-be326acfddf6"
+      );
+
+      const result = await response.json();
+      const data = result.data;
+
+      for (let d of data) {
+        const divName = document.createElement("div");
+        const divDescription = document.createElement("div");
+        const titleName = document.createElement("p");
+        titleName.classList.add("titleCategory");
+        titleName.innerText = "Nome";
+        const titleDescription = document.createElement("p");
+        titleDescription.classList.add("titleCategory");
+        titleDescription.innerText = "Descrição";
+        const category = document.createElement("div");
+        category.classList.add("category-box");
+        const icones = document.createElement("div");
+        const name = document.createElement("p");
+        name.innerText = d.name;
+        const description = document.createElement("p");
+        description.innerText = d.description;
+        const linkEditar = document.createElement("a"); 
+        //linkEditar.href = '/'
+        const linkDeletar = document.createElement("a");
+        //linkDeletar.href = 
+        const imgEditar = document.createElement("img");
+        imgEditar.src = "../../img/editar_icon.png";
+        const imgDeletar = document.createElement("img");
+        imgDeletar.src = "../../img/deletar_icon.png";
+        divName.appendChild(titleName);
+        divName.appendChild(name);
+        divDescription.appendChild(titleDescription);
+        divDescription.appendChild(description);
+        linkEditar.appendChild(imgEditar);
+        linkDeletar.appendChild(imgDeletar);
+        icones.appendChild(linkEditar);
+        icones.appendChild(linkDeletar);
+        category.appendChild(divName);
+        category.appendChild(divDescription);
+        category.appendChild(icones);
+        categories.appendChild(category);
+      }
+    } catch (error) {}
+
+    return categories;
+  }
+}
