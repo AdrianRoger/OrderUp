c253f932f9da3f58587b2bf2b7daa9c49998af2d
Adrian Roger <adrianroge@gmail.com>
2024-05-05 00:10:16 -0300

    [fix]: update imports to use index.js

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,7 +1,9 @@
 const corsMiddleware = require('./corsMiddleware');
 const upload = require('./MulterMiddleware');
+const authMiddleware = require('./AuthMiddleware')
 
 module.exports = {
   corsMiddleware,
+  authMiddleware,
   upload,
 }
