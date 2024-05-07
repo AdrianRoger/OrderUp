46e13f18a71215ff8000d03a4226f1218dc02529
Adrian Roger <adrianroge@gmail.com>
2024-05-06 22:35:11 -0300

    [fix]: rename ValidateUuidV4.js parameter

--- src/utils/ValidateUuidV4.js
+++ src/utils/ValidateUuidV4.js
@@ -1,5 +1,5 @@
 class ValidateUuidV4 {
-  static isValidUuidV4(uuid) {
+  static isValidUuidV4({ uuid }) {
     // regex to validate UUID v4
     const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
